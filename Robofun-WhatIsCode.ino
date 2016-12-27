e23ae8710204a10dbada9c35f0cfbd0796063b58
RoboFun <coders@vemny.org>
2016-12-27 10:16:34 -0500

    Add files via upload

---
+++ Robofun-WhatIsCode.ino
@@ -0,0 +1,14 @@
+const int LED = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED,HIGH);
+delay(2000);
+digitalWrite(LED,LOW);
+delay(2000);
+}
